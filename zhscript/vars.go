3469769e9aac3447fcd5aa126278cd6b72e994b8
zzzzzzzzzzz <zzzzzzzzzzzgg@gmail.com>
2016-02-21 01:51:46 +0800

    变量增时间戳。仅访问暴露在Find

--- zhscript/vars.go
+++ zhscript/vars.go
@@ -1,11 +1,14 @@
 package zhscript
 
+import "time"
+
 type Var___ struct {
 	Name string
 	Val *Val___
 	Is_lock bool
 	Annota_val *Strings___
 	Kw *Keyword___
+	Timestamp int64
 
 	Is_no_arg, Is_through bool
 	Qian_arg int
@@ -16,7 +19,11 @@ func (this *Var___) argnames_add__(s string) {
 	this.Argnames = append(this.Argnames, s)
 }
 
-func Var__(e *Em___) *Var___ {
+func (this *Var___) use__() {
+	this.Timestamp = time.Now().Unix()
+}
+
+func var__(e *Em___) *Var___ {
 	return e.Value.(*Var___)
 }
 
@@ -34,37 +41,50 @@ func (this *Val___) copy_i__(val *Val___) {
 }
 
 type Vars___ struct {
-	Ls *List___
+	ls *List___
 }
 
 func (this *Vars___) init__() {
-	this.Ls = new(List___)
+	this.ls = new(List___)
 }
 
 func (this *Vars___) get__(name string) *Var___ {
 	e := this.find__(name)
 	if e != nil {
-		return Var__(e)
+		return var__(e)
 	}
 	v := new(Var___)
 	v.Name = name
-	this.Ls.PushBack(v)
+	v.use__()
+	this.ls.PushBack(v)
 	return v
 }
 
-func (this *Vars___) find__(name string) *Em___ {
-	var e2 *Em___
-	this.Ls.Find__(func(e *Em___) bool {
-		v := Var__(e)
+func (this *Vars___) find__(name string) (e2 *Em___) {
+	this.ls.Find__(func(e *Em___) bool {
+		v := var__(e)
 		if v.Name == name {
 			e2 = e
+			v.use__()
 			return true
 		}
 		return false
 	})
-	return e2
+	return
+}
+
+func (this *Vars___) Find__(f__ func(*Var___) bool) (ok bool) {
+	this.ls.Find__(func(e *Em___) bool {
+		v := var__(e)
+		if f__(v) {
+			ok = true
+			return true
+		}
+		return false
+	})
+	return
 }
 
 func (this *Vars___) del__(e *Em___) {
-	this.Ls.Remove(e.Element)
+	this.ls.Remove(e.Element)
 }
